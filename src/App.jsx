5d7c39d59270f10e690a3095f5619af3a756c344
Mohamed Oulahguine <mohamedoulahguine@gmail.com>
2025-08-18 14:39:34 +0300

    add browers router

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,33 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  BrowserRouter,
+} from "react-router-dom";
 import Header from "./components/Header/Header";
 import Hero from "./sections/Hero/Hero";
 import About from "./sections/About/About";
 
 export default function App() {
   return (
-    <Router>
-      <Header />
-      <main>
-        <Routes>
-          <Route
-            path="/"
-            element={
-              <>
-                <Hero />
-                <About />
-              </>
-            }
-          />
-        </Routes>
-      </main>
-    </Router>
+    <BrowserRouter>
+      <Router>
+        <Header />
+        <main>
+          <Routes>
+            <Route
+              path="/"
+              element={
+                <>
+                  <Hero />
+                  <About />
+                </>
+              }
+            />
+          </Routes>
+        </main>
+      </Router>
+    </BrowserRouter>
   );
 }
