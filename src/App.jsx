73649d605067082dad655d00793b400d26fb9da6
Mohamed Oulahguine <mohamedoulahguine@gmail.com>
2025-09-09 23:04:30 +0300

    for responsive

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Header from "./components/Header/Header";
-import Footer from "./components/Footer/Footer";
 import Hero from "./sections/Hero/Hero";
 import About from "./sections/About/About";
 import Skills from "./sections/Skills/Skills";
 import Projects from "./sections/Projects/Projects";
 import Contact from "./sections/Contact/Contact";
+import Footer from "./components/Footer/Footer";
 
 export default function App() {
   return (
@@ -19,10 +19,10 @@ export default function App() {
             element={
               <>
                 <Hero />
-                {/* <About />
+                <About />
                 <Skills />
                 <Projects />
-                <Contact /> */}
+                <Contact />
               </>
             }
           />
